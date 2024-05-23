58208209150f70d2ec9ec06d9ba6476a26ab37ef
jhreyes483 <jhreyes483@misena.edu.co>
2024-05-23 13:59:13 -0500

    update

--- resources/js/components/auth/Auth.vue
+++ resources/js/components/auth/Auth.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="container-fluid">
-        <div class="col-md-6 mx-auto card card-body">
-            <div class="card-title">Sistema personal de control By: dev-jav</div>
-            <form class="kt-form" @submit.prevent="authLogin()" method="post">
+        <div class="col-md-6 mx-auto card-login card-body">
+            <div class="text-center">
+                <div class="card-title">Sistema personal de control By: dev-jav</div>
+            </div>
+            <form class="kt-form " @submit.prevent="authLogin()" method="post">
 
                 <div class="row justify-content-center">
                     <!--  
@@ -10,24 +12,18 @@
                     -->
 
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-4">
                     <input name="email" required v-model.trim="login.email" class="form-control" type="text"
                         placeholder="Correo electrónico." autocomplete="off">
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-4">
                     <input v-model.trim="login.password" name="password" type="password"
                         class="form-control form-control-last" required placeholder="Contraseña.">
                 </div>
-                <div class="text-right ml-2 mr-2 mt-1">
-                    <span @click="openModal('#forget_pass')" class="forget_pass">¿Olvidó su contraseña?</span>
-
-                </div>
-
-                <div class="kt-login__actions">
-                    <button type="submit"
-                        class="btn btn-primary kt-spinner kt-spinner--right kt-spinner--md kt-spinner--light">Ingresar</button>
-
-
+                <div class=" mt-2 text-center">
+                    <br>
+                    <button type="submit" data-color="120"
+                        class="interactive-button btn btn-login ">Ingresar</button>
                 </div>
             </form>
         </div>
@@ -46,6 +42,7 @@ export default {
     },
 
     mounted() {
+        this.efectBotons();
         console.log('llego');
     },
     methods: {
@@ -80,7 +77,97 @@ export default {
                 }
             })
         },
+        efectBotons() {
+            let buttons = document.querySelectorAll('.interactive-button');
+            buttons.forEach(button => {
+                button.addEventListener('mouseenter', function (e) {
+                    const color = this.getAttribute('data-color');
+                });
+
+                button.addEventListener('mousemove', function (e) {
+                    const rect = this.getBoundingClientRect();
+                    const x = (e.clientX - rect.left) / rect.width;
+                    const gradientStart = Math.max(0, x * 100 - 10) + '%';
+                    const gradientEnd = Math.min(100, x * 100 + 10) + '%';
+                    const color = this.getAttribute('data-color');
+                    this.style.color = `hsl(${color}, 100%, 50%)`;
+                    this.style.borderRadius = '20px';
+                    this.style.setProperty('--gradient', `linear-gradient(to right, black 0%, black ${gradientStart}, hsl(${color}, 100%, 50%) ${gradientStart}, hsl(${color}, 100%, 50%) ${gradientEnd}, black ${gradientEnd}, black 100%)`);
+                    this.style.borderImage = `var(--gradient) 1`;
+                });
+
+                button.addEventListener('mouseleave', function () {
+                    this.style.color = 'gray';
+                    this.style.borderImage = 'none';
+                    this.style.borderColor = 'black';
+                });
+            });
+        }
 
     }
 }
+
+
+let buttons = document.querySelectorAll('.interactive-button');
+
+
 </script>
+
+<style>
+.card-login {
+    margin-top: 15px;
+    padding: 100px;
+    border-radius: 1em;
+    border: solid 3px #30e0a5;
+}
+
+.btn-login {
+    margin-top: 30px;
+    border-radius: 1em;
+    margin-top: 10px;
+    padding-left: 30px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    padding-right: 30px;
+}
+
+
+.interactive-button {
+    position: relative;
+    padding: 12px 50px;
+    margin: 10px;
+    color: gray;
+    background-color: black;
+    border: 4px solid black;
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: bold;
+    overflow: hidden;
+    transition: color 0.3s;
+    border-radius: 20px;
+    /* Redondez también para el border-image */
+    --size: 0% 0%;
+}
+
+.interactive-button::after {
+    content: '';
+    position: absolute;
+    top: -4px;
+    right: -4px;
+    bottom: -4px;
+    left: -4px;
+    border-radius: 20px;
+    background: hsl(var(--hue), 100%, 50%);
+    z-index: -1;
+    transition: background-size 0.3s, background-position 0.3s ease-out;
+    background-repeat: no-repeat;
+    pointer-events: none;
+    background-size: var(--size, 0% 0%);
+    background-position: var(--position, 0% 0);
+}
+
+.interactive-button:hover {
+    border-image-slice: 1;
+    /* Asegúrate de que esta propiedad se maneje adecuadamente para preservar el border-radius */
+}
+</style>
